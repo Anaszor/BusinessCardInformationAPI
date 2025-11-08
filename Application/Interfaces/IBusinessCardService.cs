df801c64976ae9a057b7069a3e9cd3df2df91092
Anas <Anas.zoriqi@gmail.com>
2025-11-09 01:01:36 +0300

    Full-Stack upload V.1.1

--- Application/Interfaces/IBusinessCardService.cs
+++ Application/Interfaces/IBusinessCardService.cs
@@ -1,19 +1,16 @@
-﻿using BusinessCardInformationAPI.Application.DTOs;
+using BusinessCardInformationAPI.Application.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace BusinessCardInformationAPI.Application.Interfaces
 {
     public interface IBusinessCardService
     {
         Task<IEnumerable<BusinessCardDto>> GetAllAsync(string? search = null);
-
         Task<BusinessCardDto?> GetByIdAsync(int id);
-
         Task AddAsync(CreateBusinessCardDto dto);
-
         Task DeleteAsync(int id);
-
         Task<byte[]> ExportToCsvAsync();
-
         Task<byte[]> ExportToXmlAsync();
     }
 }
